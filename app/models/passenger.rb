cf6b93b7d892132e9820101cf9f6ec228ef2561a
Lola Llanes <mslourdesllanes@gmail.com>
2020-05-08 15:39:37 -0700

    added total_paid method

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,16 @@
 class Passenger < ApplicationRecord
+  validates :name, presence: true
+  validates :phone_num, presence: true
+  has_many :trips
+
+
+
+  def total_paid
+    total = 0
+    self.trips.each do |trip|
+      total += trip.cost.to_f
+    end
+  
+    return total
+  end
 end
