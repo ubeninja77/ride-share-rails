eb59a4f9d3dac38809de2d897bf6f7cdf9c5da8b
Olga Patrakova <olgapatrakova@outlook.com>
2020-05-08 22:59:17 -0700

    Earnings, rating and validations added

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,30 @@
 class Driver < ApplicationRecord
   has_many :trips
+  validates :name, presence: true
+  validates :vin, presence: true, length: { is: 17 }
+
+  def calculate_rating
+    trips_with_rating = self.trips.where.not(rating: nil)
+    trips_count = trips_with_rating.length
+    if trips_count > 0
+      rating = 0.0
+      trips_with_rating.each do |trip|
+        rating += trip.rating.to_i
+      end
+      average = rating / trips_count
+    else
+      average = 0
+    end
+    return average
+  end
+
+  def total_earnings
+    trips_with_earnings = self.trips.where.not(cost: nil)
+    earnings = 0
+    trips_with_earnings.each do |trip|
+      earnings += (trip.cost.to_i - 165) * 0.8 / 100
+    end
+    return earnings
+  end
 end
+
